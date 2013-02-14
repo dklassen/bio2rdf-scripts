1a3db4e2686aac9c08f7230cc02540703a1305ef
Dana Klassen <klassen.dana@gmail.com>
2013-02-14 10:41:44 +0000

    initial commit of modified biopax parser

--- biopax/biopax.php
+++ biopax/biopax.php
@@ -37,7 +37,8 @@ class BioPAXParser extends RDFFactory
 		$this->AddParameter('files',true,null,'all','entries to process: comma-separated list or hyphen-separated range');
 		$this->AddParameter('indir',false,null,'/tmp/biopax/download/','directory to download into and parse from');
         $this->AddParameter('outdir',false,null,'/tmp/biopax/data/','directory to place rdfized files');
-        $this->AddParameter('download',true,null,'http://pathway-commons.googlecode.com/files/pathwaycommons2-Sept2012.owl.zip','The location of the pathway commons owl file');
+        $this->AddParameter('download',false,'true|false','download remote file to indirectory');
+        $this->AddParameter('download_url',false,null,'http://pathway-commons.googlecode.com/files/pathwaycommons2-Sept2012.owl.zip');
 		$this->AddParameter('gzip',false,'true|false','true','gzip the output');
 		if($this->SetParameters($argv) == FALSE) {
 			$this->PrintParameters($argv);
@@ -55,7 +56,10 @@ class BioPAXParser extends RDFFactory
         $outdir = $this->GetParameterValue('outdir');
                 
         $file = $indir."pathwaycommons.owl.zip";
-
+        $outfile = $outdir."pathwaycommons_map.nt.gz";
+        
+        preg_match('/.*(pathwaycommons[A-Za-z0-9-]+.owl)/',$this->GetParameterValue('download_url'),$filematch);
+         echo $unzipped_file;
         // Download
         if ($this->GetParameterValue('download')){
             $download = $this->GetParameterValue('download');
@@ -63,45 +67,57 @@ class BioPAXParser extends RDFFactory
             file_put_contents($file,file_get_contents($download));
         }
 
-        // Unzip the file
+        echo "INFO: Unzipping ".$file."\n";
         $zip = new ZipArchive;
-        $result = $zip=>open($file);
-        if ( $result == TRUE ){
-            $zip->extractTo($indir."pathwaycommons.nt");
+        if ($zip->open($file) === TRUE) {
+            $zip->extractTo($indir);
             $zip->close();
-            echo "INFO: Unzipped pathway commons file";
-        }else{
-            echo "ERROR: Was unable to unzip the pathway commons file";
-            exit 1;
+        } else {
+            echo 'failed';
         }
+        echo "INFO: Unzipped pathway commons file\n";
+
+        $this->SetWriteFile($outfile,TRUE);
+        echo "INFO: Setting outfile to ".$outfile."\n";
 
         // Convert to ntriples
-        $cmd = "rapper ".$file." > ".$indir."pathwaycommons.nt";
-        exec($cmd);
+        // $cmd = "rapper ".$file." > ".$indir."pathwaycommons.nt";
+        // exec($cmd);
 
         // Generate links mapping identifiers.org to bio2rdf.org
         // this would be so much easier with sed :) 
-        $tmp_file = "/tmp/biopax_mappings.nt";
-        $th = fopen($tmp_file,'w') or die("ERROR: Unable to open temp file");
-        $handle = @fopen($indir."pathwaycommons.nt");
+       
+        // match pubmed identifiers
+        // http://identifiers.org/pubmed/11447118
+        $pubmed = "/.*pubmed\/(\d+)\"/";
+
+        // taxonomy
+        // http://identifiers.org/taxonomy/9606
+        $taxon = "/http:.*taxonomy\/(\d+)/";
+        
+        // uniprot
+        // http://identifiers.org/uniprot/Q9R1Z8
+        $uniprot = "/uniprot\/[A-Za-z0-9]+\"/";
+       
+        $tmp_file = $outdir."biopax_mappings.nt";
+        $th = fopen($unzipped_file,'w') or die("ERROR: Unable to open temp file");
+        $handle = fopen($indir."pathwaycommons2-Sept2012.owl","r");
         if ($handle) {
             while (($buffer = fgets($handle, 4096)) != false){
-                // match pubmed identifiers
-                // http://identifiers.org/pubmed/11447118
-                $pubmed = "/.*pubmed\/(\d+)\"/";
+                preg_match($taxon,$buffer,$taxon_matches);
+                if ($taxon_matches[1]){
+                    $this->AddRDF($this->QQuadO_URL("taxon:".$taxon_matches[1],"owl:sameAs",$taxon_matches[0]));
+                }
+                // echo $buffer
 
-                // taxonomy
-                // http://identifiers.org/taxonomy/9606
-                $taxon = "";
-                // uniprot
-                //  http://identifiers.org/uniprot/Q9R1Z8
-                $uniprot = "";
+
+                $this->WriteRDFBufferToWriteFile();
             }
 
             fclose($handle);
         }else{
             echo "ERROR: Unable to open file";
-            exit 1
+            exit();
         }
 
         // output triples
